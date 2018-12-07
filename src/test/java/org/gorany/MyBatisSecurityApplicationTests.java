a3f8f5ca4c2a3461325d956c29e7cda23800f349
kiddingJust <rlarkdlr@gmail.com>
2018-12-07 18:27:39 +0900

    aa

--- src/test/java/org/gorany/MyBatisSecurityApplicationTests.java
+++ src/test/java/org/gorany/MyBatisSecurityApplicationTests.java
@@ -1,5 +1,9 @@
 package org.gorany;
 
+import java.util.List;
+
+import org.gorany.domain.PartnerVO;
+import org.gorany.mapper.PartnerMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.java.Log;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log
 public class MyBatisSecurityApplicationTests {
 
 	@Autowired
 	PasswordEncoder encoder;
+	
+	@Autowired
+	PartnerMapper mapper;
 	
 	@Test
 	public void contextLoads() {
@@ -26,6 +36,16 @@ public class MyBatisSecurityApplicationTests {
 		String enPw = encoder.encode(pw);
 		
 		System.out.println(enPw);
+	}
+	
+	@Test
+	public void test() {
+		
+		PartnerVO vo = new PartnerVO();
+		String seNum = "00000000089c4f68";
+		vo.setSerialNum(seNum);
+		List<PartnerVO> list = mapper.getPartnerBySerialNum(vo);
+		log.info("" + list);
 	}
 
 }
